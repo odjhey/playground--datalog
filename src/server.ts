e57ee411f301454b465f616af3fbafd22cbb4758
Odelon Jhey Pacalso <odjhey@gmail.com>
2024-05-30 15:20:33 +0800

    add datascript

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,50 @@
-console.log("hello playground--datalog");
+import * as d from 'datascript'
+import { edn } from './utils/ednTemplateTag'
+
+// Use regular JS API for create connection and add data to DB',
+// schema is JS Object
+var schema = {
+  aka: { ':db/cardinality': ':db.cardinality/many' },
+  friend: { ':db/valueType': ':db.type/ref' },
+}
+
+var conn = d.create_conn(schema)
+var reports = []
+
+d.listen(conn, 'main', (report) => {
+  reports.push(report)
+})
+
+var datoms = [
+  {
+    ':db/id': -1,
+    name: 'Ivan',
+    age: 18,
+    aka: ['X', 'Y'],
+  },
+  {
+    ':db/id': -2,
+    name: 'Igor',
+    aka: ['Grigory', 'Egor'],
+  },
+  [':db/add', -1, 'friend', -2],
+]
+
+// Tx is Js Array of Object or Array
+d.transact(conn, datoms, 'initial info about Igor and Ivan')
+
+// report is regular JS object'
+// query mori values from conn with CLJS API
+
+// show whole DB
+
+const result = d.q(
+  edn`[:find ?n :in $ ?a 
+          :where  [?e "friend" ?f] 
+                  [?e "age" ?a] 
+                  [?f "name" ?n]]`,
+  d.db(conn),
+  18
+)
+
+console.log(result)
---
+++ src/utils/ednTemplateTag.ts
@@ -0,0 +1,12 @@
+// edn identity function
+export function ednIdentity(strings: TemplateStringsArray, ...values: any[]) {
+  // Interleave the strings with the values
+  let result = strings[0]
+  values.forEach((value, i) => {
+    result += value + strings[i + 1]
+  })
+
+  return result
+}
+
+export const edn = ednIdentity

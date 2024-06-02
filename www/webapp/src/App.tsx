b2c8ea98fbd720cef9689be20b658f94d4c870f8
Odelon Jhey Pacalso <odjhey@gmail.com>
2024-06-02 14:33:18 +0800

    add tailwind

--- www/webapp/src/App.tsx
+++ www/webapp/src/App.tsx
@@ -20,21 +20,24 @@ function Component() {
   });
   const { t, q, history, replay } = useAppState();
 
-  const query = edn`[:find ?e ?name
+  const query = edn`[:find ?e ?name ?age
         :where 
-          [?e "name" ?name]]`;
+          [?e "name" ?name]
+          [?e "age" ?age]
+        ]`;
 
   const result = (q(query) as unknown[]).map((v: any) => {
-    return { key: v[0], name: v[1] };
+    return { key: v[0], name: v[1], age: v[2] };
   });
 
   return (
-    <div>
+    <div className="m-2 flex flex-col gap-2">
       {result.map((v) => {
         return (
-          <div key={v.key}>
-            {v.key}: {v.name}
+          <div className="flex flex-row gap-2" key={v.key}>
+            {v.key}: {v.name} {v.age}
             <button
+              className="btn btn-xs btn-error"
               onClick={() => {
                 t([[":db/retractEntity", v.key]]);
               }}
@@ -44,33 +47,35 @@ function Component() {
           </div>
         );
       })}
-      name:{" "}
-      <input
-        value={user.name}
-        onChange={(e) => setUser({ ...user, name: e.target.value })}
-      />
-      age:{" "}
-      <input
-        type="number"
-        value={isNaN(user.age) ? 0 : user.age}
-        onChange={(e) => setUser({ ...user, age: parseInt(e.target.value) })}
-      />
-      <button
-        onClick={() => {
-          if (user?.name && user?.age) {
-            t([{ ":db/add": -1, ...user }]);
-          }
-        }}
-      >
-        add
-      </button>
-      <button
-        onClick={() => {
-          replay(history().map((v) => JSON.parse(v as string)));
-        }}
-      >
-        replay
-      </button>
+      <div className="flex flex-row gap-2">
+        name:{" "}
+        <input
+          value={user.name}
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
+        age:{" "}
+        <input
+          type="number"
+          value={isNaN(user.age) ? 0 : user.age}
+          onChange={(e) => setUser({ ...user, age: parseInt(e.target.value) })}
+        />
+        <button
+          onClick={() => {
+            if (user?.name && user?.age) {
+              t([{ ":db/add": -1, ...user }]);
+            }
+          }}
+        >
+          add
+        </button>
+        <button
+          onClick={() => {
+            replay(history().map((v) => JSON.parse(v as string)));
+          }}
+        >
+          replay
+        </button>
+      </div>
     </div>
   );
 }
